f64b0d5ad347c884690c15ec10bfadbc9246856e
Suryansh Pandey <yashpandey.2310@gmail.com>
2025-07-25 00:39:33 +0530

    #

--- src/components/QuestionAnswer.jsx
+++ src/components/QuestionAnswer.jsx
@@ -8,7 +8,7 @@ function QuestionAnswer({item,index}) {
       className={item.type == "q" ? "flex justify-end" : ""}>
         {item.type == "q" ? (
         <li key={index + Math.random()}
-          className="text-right p-1 border-5 dark:bg-zinc-700 bg-red-100 dark:border-zinc-700 bg-red-100 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit">
+          className="text-right p-1 border-5 dark:bg-zinc-700  dark:border-zinc-700 bg-red-100 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit">
           <Answer ans={item.text} totalResult={1} index={index}type={item.type}/>
         </li> ) : (item.text.map((ansItem, ansIndex) => (
         <li className="text-left p-1"  key={ansIndex + Math.random()}>
