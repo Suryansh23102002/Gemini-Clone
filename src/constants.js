fcc65a2aae299f078488a8fdbe4d61e128509975
Suryansh Pandey <yashpandey.2310@gmail.com>
2025-07-25 00:40:04 +0530

    #

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
 
-const API_KEY = "AIzaSyApyEm4bW5tOOThfj9juHcXkv1VwaDmDWg";
+const API_KEY = "";
 
 
 export const fullUrl = `${URL}?key=${API_KEY}`;
